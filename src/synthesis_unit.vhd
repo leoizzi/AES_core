b0fc4e95a8c12b922b96cfe054edb6d2c23dbac4
Nicole Dai Prà <nicoledaipra@MBP-di-Nicole.lan>
2021-05-31 16:55:47 +0200

    update synthesis unit

--- src/synthesis_unit.vhd
+++ src/synthesis_unit.vhd
@@ -6,7 +6,8 @@ entity synthesis_unit is
 		clk: in std_logic;
 		rst: in std_logic;
 		start: in std_logic;
-		ed: in std_logic;
+	    first_round: in std_logic;
+	    last_round: in std_logic;
 		end_block0: out std_logic;
 		end_block1: out std_logic;
 		end_block2: out std_logic;
@@ -17,7 +18,7 @@ entity synthesis_unit is
 end synthesis_unit;
 
 architecture structural of synthesis_unit is
-	component datapath is
+	component enc_datapath is
 		port (
 			clk: in std_logic;
 			rst: in std_logic;
@@ -25,13 +26,31 @@ architecture structural of synthesis_unit is
 			start: in std_logic;
 		    end_block: out std_logic;
 			
-			ed: in std_logic;
+			first_round: in std_logic;
+			last_round: in std_logic;
 
 			data_in: in std_logic_vector(127 downto 0);
 			key: in std_logic_vector(127 downto 0);
 			data_out: out std_logic_vector(127 downto 0)
 	);
-	end component datapath;
+	end component enc_datapath;
+	
+	component dec_datapath is
+		port (
+			clk: in std_logic;
+			rst: in std_logic;
+			
+			start: in std_logic;
+		    end_block: out std_logic;
+			
+			first_round: in std_logic;
+			last_round: in std_logic;
+
+			data_in: in std_logic_vector(127 downto 0);
+			key: in std_logic_vector(127 downto 0);
+			data_out: out std_logic_vector(127 downto 0)
+	);
+	end component dec_datapath;
 
 	constant din : std_logic_vector(127 downto 0) := X"6369616f6369616f6369616f6369616f";
 	constant key: std_logic_vector(127 downto 0) := X"21e3384a4055d31787bbc84654da268f";
@@ -43,7 +62,7 @@ begin
 	--din <= data_in&data_in&data_in&data_in&data_in&data_in&data_in&data_in;
 --	key <= k&k&k&k&k&k&k&k;
 	
-	dp0: datapath
+	dp0: enc_datapath
 		port map (
 			clk => clk,
 			rst => rst,
@@ -51,14 +70,15 @@ begin
 			start => start,
 			end_block => end_block0,
 
-			ed => ed,
+			first_round => first_round,
+			last_round => last_round,
 
 			data_in => din,
 			key => key,
 			data_out => tmp_out0
 		);
 		
-	dp1: datapath
+	dp1: dec_datapath
 		port map (
 			clk => clk,
 			rst => rst,
@@ -66,43 +86,13 @@ begin
 			start => start,
 			end_block => end_block1,
 
-			ed => ed,
+			first_round => first_round,
+			last_round => last_round,
 
 			data_in => din,
 			key => key,
 			data_out => tmp_out1
 		);
-		
-	dp2: datapath
-		port map (
-			clk => clk,
-			rst => rst,
 
-			start => start,
-			end_block => end_block2,
-
-			ed => ed,
-
-			data_in => din,
-			key => key,
-			data_out => tmp_out2
-		);
-		
-	dp3: datapath
-		port map (
-			clk => clk,
-			rst => rst,
-
-			start => start,
-			end_block => end_block3,
-
-			ed => ed,
-
-			data_in => din,
-			key => key,
-			data_out => tmp_out3
-		);
-
-	--data_out <= tmp_out(127 downto 96) or tmp_out(95 downto 64) or tmp_out(63 downto 32) or tmp_out(31 downto 0);
-	data_out <= (tmp_out0(127 downto 64) or tmp_out1(127 downto 64) or tmp_out2(127 downto 64) or tmp_out3(127 downto 64)) xor (tmp_out0(63 downto 0) or tmp_out1(63 downto 0) or tmp_out2(63 downto 0) or tmp_out3(63 downto 0));
+	data_out <= (tmp_out0(127 downto 64) or tmp_out1(127 downto 64)) xor (tmp_out0(63 downto 0) or tmp_out1(63 downto 0));
 end structural;
