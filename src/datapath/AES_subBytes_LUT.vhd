ff5b58de8e95078dae768d4f7b005da91dd979ea
Nicole Dai Prà <nicoledaipra@MBP-di-Nicole.homenet.telecomitalia.it>
2021-06-07 16:35:44 +0200

    datapath fully working

--- src/datapath/AES_subBytes_LUT.vhd
+++ src/datapath/AES_subBytes_LUT.vhd
@@ -37,10 +37,10 @@ entity AES_subBytes_LUT is
         clk: in std_logic;
         start: in std_logic;
         end_block: out std_logic;
-        en : out std_logic_vector(3 downto 0);
+        en : out std_logic_vector(1 downto 0);
         --Entity input/output.
         data_in : in std_logic_vector(127 downto 0);
-        data_out: out std_logic_vector(31 downto 0);
+        data_out: out std_logic_vector(63 downto 0);
         --Lookup memory interface.
         look_0_addr: out std_logic_vector(7 downto 0);
         look_0_data: in std_logic_vector(7 downto 0);
@@ -49,13 +49,21 @@ entity AES_subBytes_LUT is
         look_2_addr: out std_logic_vector(7 downto 0);
         look_2_data: in std_logic_vector(7 downto 0);
         look_3_addr: out std_logic_vector(7 downto 0);
-        look_3_data: in std_logic_vector(7 downto 0));
+        look_3_data: in std_logic_vector(7 downto 0);
+        
+        look_4_addr: out std_logic_vector(7 downto 0);
+        look_4_data: in std_logic_vector(7 downto 0);
+        look_5_addr: out std_logic_vector(7 downto 0);
+        look_5_data: in std_logic_vector(7 downto 0);
+        look_6_addr: out std_logic_vector(7 downto 0);
+        look_6_data: in std_logic_vector(7 downto 0);
+        look_7_addr: out std_logic_vector(7 downto 0);
+        look_7_data: in std_logic_vector(7 downto 0));
 end AES_subBytes_LUT;
 
 architecture Behavioral of AES_subBytes_LUT is
 type state is (RESET, IDLE, LOOKUP);
 signal c_state, n_state: state;
-signal c_cnt, n_cnt : std_logic_vector(1 downto 0);
 begin
     
     process(clk)
@@ -63,32 +71,37 @@ begin
         if (clk = '1' and clk'event) then
             if (rst = '1') then
                 c_state <= RESET;
-                c_cnt <= (others => '0');
             else
                 c_state <= n_state;
-                c_cnt <= n_cnt;
             end if;
         end if;
     end process;
 
-    process(c_state, start, data_in, c_cnt, look_0_data, look_1_data, look_2_data, look_3_data)
+    process(c_state, start, data_in, look_0_data, look_1_data, look_2_data, look_3_data, look_4_data, look_5_data, look_6_data, look_7_data)
     begin
-    n_cnt <= (others => '0');
     n_state <= c_state;
     data_out <= (others => '0');
     look_0_addr <= (others => '0');
     look_1_addr <= (others => '0');
     look_2_addr <= (others => '0');
     look_3_addr <= (others => '0');
+    look_4_addr <= (others => '0');
+    look_5_addr <= (others => '0');
+    look_6_addr <= (others => '0');
+    look_7_addr <= (others => '0');
     end_block <= '0';
+    en <= "00";
     case c_state is 
         when RESET =>
             n_state <= IDLE;
-            n_cnt <= c_cnt;
             look_0_addr <= (others => '0');
             look_1_addr <= (others => '0');
             look_2_addr <= (others => '0');
             look_3_addr <= (others => '0');
+            look_4_addr <= (others => '0');
+            look_5_addr <= (others => '0');
+            look_6_addr <= (others => '0');
+            look_7_addr <= (others => '0');
             data_out <= (others => '0');
             end_block <= '0';
         when IDLE => 
@@ -98,51 +111,40 @@ begin
                 look_1_addr <= data_in(15 downto 8);
                 look_2_addr <= data_in(23 downto 16);
                 look_3_addr <= data_in(31 downto 24);
-                data_out <= look_3_data&look_2_data&look_1_data&look_0_data;
+                
+                look_4_addr <= data_in(39 downto 32);
+                look_5_addr <= data_in(47 downto 40);
+                look_6_addr <= data_in(55 downto 48);
+                look_7_addr <= data_in(63 downto 56);
+                data_out <= look_7_data&look_6_data&look_5_data&look_4_data&look_3_data&look_2_data&look_1_data&look_0_data;
+                en <= "01";
             else
                 n_state <= c_state ;
                 look_0_addr <= (others => '0');
                 look_1_addr <= (others => '0');
                 look_2_addr <= (others => '0');
                 look_3_addr <= (others => '0');
+                look_4_addr <= (others => '0');
+                look_5_addr <= (others => '0');
+                look_6_addr <= (others => '0');
+                look_7_addr <= (others => '0');
+                en <= "00";
             end if;
             end_block <= '0';
-            n_cnt <= c_cnt; 
         when LOOKUP =>
-            case c_cnt is 
-                when "00" =>
-                    --Next 32 request.
-                    look_0_addr <= data_in(39 downto 32);
-                    look_1_addr <= data_in(47 downto 40);
-                    look_2_addr <= data_in(55 downto 48);
-                    look_3_addr <= data_in(63 downto 56);
-                    n_state <= c_state;
-                when "01" =>
-                    --Next 32 request.
-                    look_0_addr <= data_in(71 downto 64);
-                    look_1_addr <= data_in(79 downto 72);
-                    look_2_addr <= data_in(87 downto 80);
-                    look_3_addr <= data_in(95 downto 88);
-                    n_state <= c_state;
-                when "10" =>
-                    --Next 32 request.
-                    look_0_addr <= data_in(103 downto 96);
-                    look_1_addr <= data_in(111 downto 104);
-                    look_2_addr <= data_in(119 downto 112);
-                    look_3_addr <= data_in(127 downto 120);
-                    n_state <= IDLE;
-                    end_block <= '1';
-                 
-                when OTHERS => 
-            end case;
-            
-            data_out <= look_3_data&look_2_data&look_1_data&look_0_data;
-            n_cnt <= std_logic_vector(unsigned(c_cnt)+1);
+                look_0_addr <= data_in(71 downto 64);
+                look_1_addr <= data_in(79 downto 72);
+                look_2_addr <= data_in(87 downto 80);
+                look_3_addr <= data_in(95 downto 88);
+                look_4_addr <= data_in(103 downto 96);
+                look_5_addr <= data_in(111 downto 104);
+                look_6_addr <= data_in(119 downto 112);
+                look_7_addr <= data_in(127 downto 120);
+                data_out <= look_7_data&look_6_data&look_5_data&look_4_data&look_3_data&look_2_data&look_1_data&look_0_data;
+                en <= "10";
+                end_block <= '1';
+                n_state <= IDLE;
     end case;
     end process;
     
-en <= "0001" when c_cnt = "00" else
-      "0010" when c_cnt = "01" else
-      "0100" when c_cnt = "10" else
-      "1000";  
 end Behavioral;
--- src/datapath/enc_datapath.vhd
+++ src/datapath/enc_datapath.vhd
@@ -12,6 +12,9 @@ entity enc_datapath is
 	    first_round: in std_logic;
 	    last_round: in std_logic;
 
+	    -- enable for the register which is input to the SubBytes step
+	    en_ff2: in std_logic;
+
 		data_in: in std_logic_vector(127 downto 0);
 		key: in std_logic_vector(127 downto 0);
 		data_out: out std_logic_vector(127 downto 0)
@@ -42,24 +45,33 @@ architecture structural of enc_datapath is
 	
 	--SubBytesLUT anticipates the ROM output by 1 cc. 
 	component AES_subBytes_LUT is
-      Port (--FSM data.
-            rst: in std_logic;
-            clk: in std_logic;
-            start: in std_logic;
-            end_block: out std_logic;
-            en : out std_logic_vector(3 downto 0);
-            --Entity input/output.
-            data_in : in std_logic_vector(127 downto 0);
-            data_out: out std_logic_vector(31 downto 0);
-            --Lookup memory interface.
-            look_0_addr: out std_logic_vector(7 downto 0);
-            look_0_data: in std_logic_vector(7 downto 0);
-            look_1_addr: out std_logic_vector(7 downto 0);
-            look_1_data: in std_logic_vector(7 downto 0);
-            look_2_addr: out std_logic_vector(7 downto 0);
-            look_2_data: in std_logic_vector(7 downto 0);
-            look_3_addr: out std_logic_vector(7 downto 0);
-            look_3_data: in std_logic_vector(7 downto 0));
+     Port (--FSM data.
+        rst: in std_logic;
+        clk: in std_logic;
+        start: in std_logic;
+        end_block: out std_logic;
+        en : out std_logic_vector(1 downto 0);
+        --Entity input/output.
+        data_in : in std_logic_vector(127 downto 0);
+        data_out: out std_logic_vector(63 downto 0);
+        --Lookup memory interface.
+        look_0_addr: out std_logic_vector(7 downto 0);
+        look_0_data: in std_logic_vector(7 downto 0);
+        look_1_addr: out std_logic_vector(7 downto 0);
+        look_1_data: in std_logic_vector(7 downto 0);
+        look_2_addr: out std_logic_vector(7 downto 0);
+        look_2_data: in std_logic_vector(7 downto 0);
+        look_3_addr: out std_logic_vector(7 downto 0);
+        look_3_data: in std_logic_vector(7 downto 0);
+        
+        look_4_addr: out std_logic_vector(7 downto 0);
+        look_4_data: in std_logic_vector(7 downto 0);
+        look_5_addr: out std_logic_vector(7 downto 0);
+        look_5_data: in std_logic_vector(7 downto 0);
+        look_6_addr: out std_logic_vector(7 downto 0);
+        look_6_data: in std_logic_vector(7 downto 0);
+        look_7_addr: out std_logic_vector(7 downto 0);
+        look_7_data: in std_logic_vector(7 downto 0));
     end component AES_subBytes_LUT;
 
     component EncShiftRows is
@@ -84,11 +96,13 @@ architecture structural of enc_datapath is
 		);
 	end component add_round_keys;
 	
-	signal sub_bytes_en: std_logic_vector(3 downto 0);
+	signal sub_bytes_en: std_logic_vector(1 downto 0);
 	signal look_0_addr, look_1_addr, look_2_addr, look_3_addr: std_logic_vector(7 downto 0);
-	signal look_0_data_en, look_1_data_en, look_2_data_en, look_3_data_en: std_logic_vector(7 downto 0);
-	signal look_0_data, look_1_data, look_2_data, look_3_data: std_logic_vector(7 downto 0);
-	signal sub_bytes_data_out: std_logic_vector(31 downto 0);
+    signal look_4_addr, look_5_addr, look_6_addr, look_7_addr: std_logic_vector(7 downto 0);
+    
+    signal look_0_data, look_1_data, look_2_data, look_3_data: std_logic_vector(7 downto 0);
+	signal look_4_data, look_5_data, look_6_data, look_7_data: std_logic_vector(7 downto 0);
+	signal sub_bytes_data_out: std_logic_vector(63 downto 0);
 	signal sub_bytes_data_in: std_logic_vector(127 downto 0);
 	signal shift_rows_data_in, shift_rows_data_out: std_logic_vector(127 downto 0);
 	signal mix_column_in, mix_column_out: std_logic_vector(127 downto 0);
@@ -96,33 +110,53 @@ architecture structural of enc_datapath is
 	signal ff1_out, ff2_out: std_logic_vector(127 downto 0);
 	signal add_rk_in_ctrl: std_logic_vector(1 downto 0);
 begin
-    look_0_data <= look_0_data_en;
-    look_1_data <= look_1_data_en;
-    look_2_data <= look_2_data_en;
-    look_3_data <= look_3_data_en;
-		
 	enc_rom_0: lut_rom
 		port map (
 			Address => look_0_addr,
-			Q => look_0_data_en
+			Q => look_0_data
 		);
 		
 	enc_rom_1: lut_rom
 		port map (
 			Address => look_1_addr,
-			Q => look_1_data_en
+			Q => look_1_data
 		);
 		
 	enc_rom_2: lut_rom
 		port map (
 			address => look_2_addr,
-			q => look_2_data_en
+			q => look_2_data
 		);
 		
 	enc_rom_3: lut_rom
 		port map (
 			Address => look_3_addr,
-			Q => look_3_data_en
+			Q => look_3_data
+		);
+		
+		
+    enc_rom_4: lut_rom
+		port map (
+			Address => look_4_addr,
+			Q => look_4_data
+		);
+		
+	enc_rom_5: lut_rom
+		port map (
+			address => look_5_addr,
+			q => look_5_data
+		);
+		
+	enc_rom_6: lut_rom
+		port map (
+			Address => look_6_addr,
+			Q => look_6_data
+		);
+		
+    enc_rom_7: lut_rom
+		port map (
+			Address => look_7_addr,
+			Q => look_7_data
 		);
 		
 	sb: AES_subBytes_LUT
@@ -143,56 +177,39 @@ begin
 	        look_2_addr => look_2_addr,
 	        look_2_data => look_2_data,
 	        look_3_addr => look_3_addr,
-	        look_3_data => look_3_data
+	        look_3_data => look_3_data,
+	        
+	        look_4_addr => look_4_addr,
+	        look_4_data => look_4_data,
+	        look_5_addr => look_5_addr,
+	        look_5_data => look_5_data,
+	        look_6_addr => look_6_addr,
+	        look_6_data => look_6_data,
+	        look_7_addr => look_7_addr,
+	        look_7_data => look_7_data
 		);
-
-	ff0_0: reg_en
+	  ff0_0: reg_en
 		generic map (
-			N => 32
+			N => 64
 		)
 		port map (
 			clk => clk,
 			rst => rst,
 			en => sub_bytes_en(0),
 			d => sub_bytes_data_out,
-			q => shift_rows_data_in(31 downto 0)
+			q => shift_rows_data_in(63 downto 0)
 		);
 
 	ff0_1: reg_en
 		generic map (
-			N => 32
+			N => 64
 		)
 		port map (
 			clk => clk,
 			rst => rst,
 			en => sub_bytes_en(1),
 			d => sub_bytes_data_out,
-			q => shift_rows_data_in(63 downto 32)
-		);
-
-
-	ff0_2: reg_en
-		generic map (
-			N => 32
-		)
-		port map (
-			clk => clk,
-			rst => rst,
-			en => sub_bytes_en(2),
-			d => sub_bytes_data_out,
-			q => shift_rows_data_in(95 downto 64)
-		);
-
-	ff0_3: reg_en
-		generic map (
-			N => 32
-		)
-		port map (
-			clk => clk,
-			rst => rst,
-			en => sub_bytes_en(3),
-			d => sub_bytes_data_out,
-			q => shift_rows_data_in(127 downto 96)
+			q => shift_rows_data_in(127 downto 64)
 		);
 
 	sr: EncShiftRows
@@ -233,7 +250,7 @@ begin
 			key => key,
 			data_out => add_round_key_out
 		);
-
+	
 	ff2: reg_en
 		generic map (
 			N => 128
@@ -241,7 +258,7 @@ begin
 		port map (
 			clk => clk,
 			rst => rst,
-			en => '1',
+			en => en_ff2,
 			d => add_round_key_out,
 			q => ff2_out
 		);
