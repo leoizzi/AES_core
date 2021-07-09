d6aa206cf45c87c46a804a18fa485e84aef65ab5
lollo <lollogrottesi@gmail.com>
2021-07-09 10:39:10 +0200

    AES_FPGA decryption

--- Firmware/Project/Inc/AES_FPGA.h
+++ Firmware/Project/Inc/AES_FPGA.h
@@ -27,15 +27,32 @@
 #define AES_KEY_INIT_ERROR 			    (-3)
 
 
-/** \brief Generate the encryption keys, share all keys with the FPGA and perform encryption.
+
+/** \brief Initialize the context for the encription and/or decription operations..
  *  \param ctx Context to be initialized. (Free context).
  *  \param Key 256 bit Key.
- *  \param Clear_data data to be encrypted.
- *  \param Clear_data data to be encrypted.
- *  \return Returns 1 on success, error code on error.
+ *  \param Clear_data plain text i.e. data to be encrypted.
+ *  \result ctx context has been initialized.
+ *  \return Returns AES_FPGA_RES_OK on success, error code on error.
  */
-AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx, const uint8_t *Key, const uint8_t *clear_data, uint8_t *encr_data);
+AES_FPGA_RETURN_CODE AES_FPGA_256_init(B5_tAesCtx *ctx, const uint8_t *Key);
 
+/** \brief Encrypt data using FPGA accelaration.
+ *  \param ctx Context already initialized.
+ *  \param Clear_data plain text i.e. data to be encrypted.
+ *  \result encr_data encrypted plain text.
+ *  \return Returns AES_FPGA_RES_OK on success, error code on error.
+ */
+AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx/*, const uint8_t *Key*/, const uint8_t *clear_data, uint8_t *encr_data);
+
+
+/** \brief Decrypt data using FPGA accelaration.
+ *  \param ctx Context already initialized.
+ *  \param encr_data Encripted data pointer
+ *  \result clear_data plain text.
+ *  \return Returns AES_FPGA_RES_OK on success, error code on error.
+ */
+AES_FPGA_RETURN_CODE AES_FPGA_256_decrypt(B5_tAesCtx *ctx, uint8_t *encr_data, uint8_t *clear_data);
 #endif /* INC_AES_FPGA_H_ */
 
 
--- Firmware/Project/Src/AES_FPGA.c
+++ Firmware/Project/Src/AES_FPGA.c
@@ -10,10 +10,17 @@
 #include "aes256.h"
 #include "Fpgaipm.h"
 
-AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx, const uint8_t *Key, const uint8_t *clear_data, uint8_t *encr_data){
-	int8_t r;
+AES_FPGA_RETURN_CODE AES_FPGA_256_init(B5_tAesCtx *ctx, const uint8_t *Key){
+	//Init the ctx structure.
 	if (B5_Aes256_Init(&ctx, (uint8_t*) &Key, sizeof(Key), B5_AES256_CTR) != B5_AES256_RES_OK)
 		return AES_KEY_INIT_ERROR;
+	//Return OK.
+	return AES_FPGA_RES_OK;
+}
+
+AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx, const uint8_t *clear_data, uint8_t *encr_data){
+
+	int8_t r;
 
 	//Here the ctx contains all key for the encription.
 	//Open communication with FPGA.
@@ -33,11 +40,11 @@ AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx, const uint8_t *Key, c
 
 	r = FPGA_IPM_open(
 				AES_CORE_ID,		  //AES_ID.
-				ENCRYPT_OPCODE,       //SHARE GENERATED KEY OPERATION.
+				ENCRYPT_OPCODE,       //ENCRYPT OPERATION.
 				0,					  //POLLING MODE.
 				0);				      //NO ACK.
 	if (r == 0)
-			return  AES_FPGA_OPEN_ERROR;
+		return  AES_FPGA_OPEN_ERROR;
 
 	//COM correctly opened.
 	//TODO - SEND ALL DATA SECTION ACCORDING WITH FPGA AND READ THE RESPONSE.
@@ -47,4 +54,42 @@ AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx, const uint8_t *Key, c
 
 	return AES_FPGA_RES_OK;
 
+}
+
+
+AES_FPGA_RETURN_CODE AES_FPGA_256_decrypt(B5_tAesCtx *ctx, uint8_t *encr_data, uint8_t *clear_data){
+
+	int8_t r;
+
+	//Here the ctx contains all key for the encription.
+	//Open communication with FPGA.
+	r = FPGA_IPM_open(
+			AES_CORE_ID,		  //AES_ID.
+			SHARE_GEN_KEY_OPCODE, //SHARE GENERATED KEY OPERATION.
+			0,					  //POLLING MODE.
+			0);				      //NO ACK.
+	if (r == 0)
+		return  AES_FPGA_OPEN_ERROR;
+
+	//COM correctly opened.
+	//TODO - SEND ALL KEYS ACCORDING WITH FPGA.
+	FPGA_IPM_write(AES_CORE_ID, 0, &ctx->rk);
+
+	FPGA_IPM_close(AES_CORE_ID);
+
+	r = FPGA_IPM_open(
+				AES_CORE_ID,		  //AES_ID.
+				DECRYPT_OPCODE,       //DECRYPT OPERATION.
+				0,					  //POLLING MODE.
+				0);				      //NO ACK.
+	if (r == 0)
+		return  AES_FPGA_OPEN_ERROR;
+
+	//COM correctly opened.
+	//TODO - SEND ALL DATA SECTION ACCORDING WITH FPGA AND READ THE RESPONSE.
+	FPGA_IPM_write(AES_CORE_ID, 0, &encr_data);
+	FPGA_IPM_read(AES_CORE_ID, 0, &clear_data);
+	FPGA_IPM_close(AES_CORE_ID);
+
+	return AES_FPGA_RES_OK;
 }
