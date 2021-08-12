f2ac85b1c83c896f3333fe0a4ff878979a4fe116
lollo <lollogrottesi@gmail.com>
2021-08-12 17:41:42 +0200

    AES FPGA SDK

--- Firmware/Project/Inc/AES_FPGA.h
+++ Firmware/Project/Inc/AES_FPGA.h
@@ -15,44 +15,72 @@
 
 #define AES_FPGA_RETURN_CODE int8_t
 
-/*FPGA Communication*/
-#define AES_CORE_ID 			(   1)
-#define SHARE_GEN_KEY_OPCODE 	(0x01)
-#define ENCRYPT_OPCODE 			(0x02)
-#define DECRYPT_OPCODE 			(0x03)
-/*Return type declarations.*/
+/* AES OPERATING MODE */
+#define AES_MODE_ENCRYPT (0x00)
+#define AES_MODE_DECRYPT (0x01)
+
+/* COSTANTS */
+#define FPGA_LOCK 0xFFFF
+
+/* FPGA Communication */
+#define AES_CORE_ID 			(1)
+
+/* OPCODES */
+#define IDLE 					(0x00)
+#define WAIT_TR_CLOSE			(0x01)
+#define ALG_SEL					(0x02)
+#define KEY_TRANSFER			(0x04)
+#define KEY_WRITE				(0x05)
+#define DATA_TX					(0x08)
+#define ENCRYPTION				(0x10)
+#define DECRYPTION				(0x11)
+#define ENC_DATA_TX				(0x18)
+#define DEC_DATA_TX				(0x19)
+#define CORE_DONE				(0x20)
+
+/* FPGA ADDRESSES */
+#define LOCK_ADDR				(0x01)
+#define ALG_SEL_ADDR            (0x08)
+#define WRITE_BASE_ADDR			(0x01 << 3)
+
+/* RETURN TYPE */
 #define AES_FPGA_RES_OK				    ( 1)
+#define FPGA_TRANSACTION_OK				( 2)
 #define AES_FPGA_RES_INVALID_ARGUMENT   (-1)
 #define AES_FPGA_OPEN_ERROR 			(-2)
-#define AES_KEY_INIT_ERROR 			    (-3)
+#define AES_FPGA_CLOSE_ERROR 			(-3)
+#define AES_KEY_INIT_ERROR 			    (-4)
+#define FPGA_ERROR						(-5)
 
 
 
-/** \brief Initialize the context for the encription and/or decription operations..
+
+/** \brief Initialize the context for the encription.
  *  \param ctx Context to be initialized. (Free context).
- *  \param Key 256 bit Key.
- *  \param Clear_data plain text i.e. data to be encrypted.
+ *  \param Key pointer.
+ *  \param keySize 128,192 or 256 bit. This parameter is used to identify the AES MODE.
+ *  \param aes_mode AES operating mode. Supported values are: AES_MODE_ENCRPT for encryption and AES_MODE_DECRYPT for decryption.
  *  \result ctx context has been initialized.
  *  \return Returns AES_FPGA_RES_OK on success, error code on error.
  */
-AES_FPGA_RETURN_CODE AES_FPGA_256_init(B5_tAesCtx *ctx, const uint8_t *Key);
+AES_FPGA_RETURN_CODE AES_FPGA_setup(B5_tAesCtx *ctx, const uint8_t *Key, int16_t keySize, uint8_t aes_mode);
 
 /** \brief Encrypt data using FPGA accelaration.
- *  \param ctx Context already initialized.
- *  \param Clear_data plain text i.e. data to be encrypted.
- *  \result encr_data encrypted plain text.
+ *  \param  ctx Context already initialized.
+ *  \param  plaintext 128 bit plain text.
+ *  \result cyphertext 128 bit cypher text.
  *  \return Returns AES_FPGA_RES_OK on success, error code on error.
  */
-AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx/*, const uint8_t *Key*/, const uint8_t *clear_data, uint8_t *encr_data);
+AES_FPGA_RETURN_CODE AES_FPGA_encrypt(B5_tAesCtx *ctx, uint8_t *plaintext, uint8_t plaintextSize, uint8_t *cyphertext);
 
 
 /** \brief Decrypt data using FPGA accelaration.
- *  \param ctx Context already initialized.
- *  \param encr_data Encripted data pointer
- *  \result clear_data plain text.
+ *  \param  ctx Context already initialized.
+ *  \param  cyphertext 128 bit cypher text.
+ *  \result plaintext 128 bit plain text.
  *  \return Returns AES_FPGA_RES_OK on success, error code on error.
  */
-AES_FPGA_RETURN_CODE AES_FPGA_256_decrypt(B5_tAesCtx *ctx, uint8_t *encr_data, uint8_t *clear_data);
+AES_FPGA_RETURN_CODE AES_FPGA_decrypt(B5_tAesCtx *ctx, uint8_t *cyphertext, uint8_t cyphertextSize,uint8_t *plaintext);
 #endif /* INC_AES_FPGA_H_ */
 
 
--- Firmware/Project/Src/AES_FPGA.c
+++ Firmware/Project/Src/AES_FPGA.c
@@ -10,86 +10,205 @@
 #include "aes256.h"
 #include "Fpgaipm.h"
 
-AES_FPGA_RETURN_CODE AES_FPGA_256_init(B5_tAesCtx *ctx, const uint8_t *Key){
-	//Init the ctx structure.
-	if (B5_Aes256_Init(&ctx, (uint8_t*) &Key, sizeof(Key), B5_AES256_CTR) != B5_AES256_RES_OK)
+AES_FPGA_RETURN_CODE AES_FPGA_setup(B5_tAesCtx *ctx, const uint8_t *Key, int16_t keySize, uint8_t aes_mode){
+
+	int8_t r;
+	FPGA_IPM_ADDRESS base_addr = WRITE_BASE_ADDR;
+
+	if (aes_mode == AES_MODE_ENCRYPT)
+		r = B5_Aes256_Init(ctx, Key, keySize, B5_AES256_CBC_ENC);
+	else if (aes_mode == AES_MODE_DECRYPT)
+		r = B5_Aes256_Init(ctx, Key, keySize, B5_AES256_CBC_DEC);
+	else
 		return AES_KEY_INIT_ERROR;
+
+	//Init the ctx structure.
+	if (r != B5_AES256_RES_OK)
+		return AES_KEY_INIT_ERROR;
+
+//Select the OPERATING MODE OF THE FPGA: 128, 192, 256.
+
+	//Open communication with FPGA.
+	r = FPGA_IPM_open(AES_CORE_ID,		  //AES_ID.
+					  ALG_SEL, 			  //ALGORITHM SELECTION OPCODE.
+					  0,				  //POLLING MODE.
+					  0);				  //NO ACK.
+	if (r != 0)
+		return  FPGA_ERROR;
+	//Transaction correctly opened.
+
+	FPGA_IPM_DATA algo_sel = (FPGA_IPM_DATA)(ctx->Nr + 1);
+
+	//Write the algorithm selection.
+	FPGA_IPM_write(AES_CORE_ID,			  //AES_ID.
+				   ALG_SEL_ADDR,		  //FPGA ADDRESS.
+				   &algo_sel);            //16-bit Data.
+
+	//Close transaction.
+	if (FPGA_IPM_close(AES_CORE_ID) != 0)
+		return FPGA_ERROR;
+
+//Transfer the keys in the context.
+
+	//Open communication with FPGA.
+	r = FPGA_IPM_open(AES_CORE_ID,		  //AES_ID.
+					  KEY_TRANSFER,       //SHARE KEY OPCODE.
+					  0,				  //POLLING MODE.
+					  0);				  //NO ACK.
+	if (r != 0)
+		return  FPGA_ERROR;
+
+	FPGA_IPM_DATA rk_msb, rk_lsb;
+
+	uint8_t key_cnt = 0;
+	for (int i=0; i < (algo_sel << 2); i++){ //4*(Nr + 1)
+		/* ctx-> rk[i] is 32-bit and IPM data is 16 bit */
+		rk_msb = (FPGA_IPM_DATA) ((ctx->rk[i] >> 16) & 0x0000FFFF);
+		rk_lsb = (FPGA_IPM_DATA) (ctx->rk[i] & 0x0000FFFF);
+
+		FPGA_IPM_write(AES_CORE_ID,			  			  //AES_ID.
+					  (base_addr + (key_cnt % 8)),		  //FPGA ADDRESS.
+					   &rk_lsb);                          //16-bit Data.
+		key_cnt ++;
+
+		FPGA_IPM_write(AES_CORE_ID,			  			  //AES_ID.
+					  (base_addr + (key_cnt % 8)),		  //FPGA ADDRESS.
+					   &rk_msb);                          //16-bit Data.
+		key_cnt ++;
+	}
+	//Close transaction.
+	if (FPGA_IPM_close(AES_CORE_ID) != 0)
+		return FPGA_ERROR;
+
 	//Return OK.
 	return AES_FPGA_RES_OK;
 }
 
-AES_FPGA_RETURN_CODE AES_FPGA_256_encrypt(B5_tAesCtx *ctx, const uint8_t *clear_data, uint8_t *encr_data){
+AES_FPGA_RETURN_CODE AES_FPGA_encrypt(B5_tAesCtx *ctx, uint8_t *plaintext, uint8_t plaintextSize, uint8_t *cyphertext) {
 
 	int8_t r;
+	FPGA_IPM_ADDRESS base_addr = WRITE_BASE_ADDR;
 
-	//Here the ctx contains all key for the encription.
+	if (plaintextSize != 16) //plaintext must be 128 bit length.
+		return AES_FPGA_RES_INVALID_ARGUMENT;
+
+//Transmit the data to encrypt.
 	//Open communication with FPGA.
-	r = FPGA_IPM_open(
-			AES_CORE_ID,		  //AES_ID.
-			SHARE_GEN_KEY_OPCODE, //SHARE GENERATED KEY OPERATION.
-			0,					  //POLLING MODE.
-			0);				      //NO ACK.
-	if (r == 0)
-		return  AES_FPGA_OPEN_ERROR;
+	r = FPGA_IPM_open(AES_CORE_ID,		  //AES_ID.
+					  DATA_TX,            //DATA TX OPCODE.
+					  0,				  //POLLING MODE.
+					  0);				  //NO ACK.
+	if (r != 0)
+		return  FPGA_ERROR;
 
-	//COM correctly opened.
-	//TODO - SEND ALL KEYS ACCORDING WITH FPGA.
-	FPGA_IPM_write(AES_CORE_ID, 0, &ctx->rk);
+	FPGA_IPM_DATA tx_data;
 
-	FPGA_IPM_close(AES_CORE_ID);
+	//Transmit 128-bit data.
+	for (int i=0; i<8; i++){
+		tx_data = (FPGA_IPM_DATA) ( (plaintext[i] << 8) | plaintext[i+1] );
+		FPGA_IPM_write(AES_CORE_ID,		   //AES_ID.
+					  (base_addr + i),	   //FPGA ADDRESS.
+					   &tx_data);		   //NO ACK.
+	}
+	//Close transaction.
+	if (FPGA_IPM_close(AES_CORE_ID) != 0)
+		return FPGA_ERROR;
+//Perform encription and read back the result from the FPGA.
+	//Open communication with FPGA.
+	r = FPGA_IPM_open(AES_CORE_ID,		  //AES_ID.
+					  ENCRYPTION,         //ENCRYPTION OPCODE.
+					  0,				  //POLLING MODE.
+					  0);				  //NO ACK.
+	if (r != 0)
+		return  FPGA_ERROR;
 
-	r = FPGA_IPM_open(
-				AES_CORE_ID,		  //AES_ID.
-				ENCRYPT_OPCODE,       //ENCRYPT OPERATION.
-				0,					  //POLLING MODE.
-				0);				      //NO ACK.
-	if (r == 0)
-		return  AES_FPGA_OPEN_ERROR;
+	//Write the lock.
+	FPGA_IPM_DATA lock = FPGA_LOCK;
+	FPGA_IPM_write(AES_CORE_ID,		   //AES_ID.
+			       LOCK_ADDR,	       //FPGA ADDRESS.
+				   &lock);		       //NO ACK.
 
-	//COM correctly opened.
-	//TODO - SEND ALL DATA SECTION ACCORDING WITH FPGA AND READ THE RESPONSE.
-	FPGA_IPM_write(AES_CORE_ID, 0, &clear_data);
-	FPGA_IPM_read(AES_CORE_ID, 0, &encr_data);
-	FPGA_IPM_close(AES_CORE_ID);
+	//Wait until FPGA ends the encryption.
+	while (lock != FPGA_LOCK)
+		FPGA_IPM_read(AES_CORE_ID, LOCK_ADDR, &lock);
+
+	//FPGA computation finished, read back the encrypted data.
+	FPGA_IPM_DATA rcvd;
+	for (int i=0; i<8; i++){
+		FPGA_IPM_read(AES_CORE_ID, (base_addr + i), &rcvd);
+		cyphertext[i]   = (uint8_t) ((rcvd >> 8) & 0x00FF);
+		cyphertext[i+1] = (uint8_t) (rcvd & 0x00FF);
+	}
+
+	//Close transaction.
+	if (FPGA_IPM_close(AES_CORE_ID) != 0)
+		return FPGA_ERROR;
 
 	return AES_FPGA_RES_OK;
 
 }
 
 
-AES_FPGA_RETURN_CODE AES_FPGA_256_decrypt(B5_tAesCtx *ctx, uint8_t *encr_data, uint8_t *clear_data){
+AES_FPGA_RETURN_CODE AES_FPGA_decrypt(B5_tAesCtx *ctx, uint8_t *cyphertext, uint8_t cyphertextSize, uint8_t *plaintext) {
 
 	int8_t r;
+	FPGA_IPM_ADDRESS base_addr = WRITE_BASE_ADDR;
 
-	//Here the ctx contains all key for the encription.
+	if (plaintextSize != 16) //plaintext must be 128 bit length.
+		return AES_FPGA_RES_INVALID_ARGUMENT;
+
+//Transmit the data to encrypt.
 	//Open communication with FPGA.
-	r = FPGA_IPM_open(
-			AES_CORE_ID,		  //AES_ID.
-			SHARE_GEN_KEY_OPCODE, //SHARE GENERATED KEY OPERATION.
-			0,					  //POLLING MODE.
-			0);				      //NO ACK.
-	if (r == 0)
-		return  AES_FPGA_OPEN_ERROR;
+	r = FPGA_IPM_open(AES_CORE_ID,		  //AES_ID.
+					  DATA_TX,            //DATA TX OPCODE.
+					  0,				  //POLLING MODE.
+					  0);				  //NO ACK.
+	if (r != 0)
+		return  FPGA_ERROR;
 
-	//COM correctly opened.
-	//TODO - SEND ALL KEYS ACCORDING WITH FPGA.
-	FPGA_IPM_write(AES_CORE_ID, 0, &ctx->rk);
+	FPGA_IPM_DATA tx_data;
 
-	FPGA_IPM_close(AES_CORE_ID);
+	//Transmit 128-bit data.
+	for (int i=0; i<8; i++){
+		tx_data = (FPGA_IPM_DATA) ( (cyphertextSize[i] << 8) | cyphertextSize[i+1] );
+		FPGA_IPM_write(AES_CORE_ID,		   //AES_ID.
+					  (base_addr + i),	   //FPGA ADDRESS.
+					   &tx_data);		   //NO ACK.
+	}
+	//Close transaction.
+	if (FPGA_IPM_close(AES_CORE_ID) != 0)
+		return FPGA_ERROR;
+//Perform encription and read back the result from the FPGA.
+	//Open communication with FPGA.
+	r = FPGA_IPM_open(AES_CORE_ID,		  //AES_ID.
+					  DECRYPTION,         //DECRYPTION OPCODE.
+					  0,				  //POLLING MODE.
+					  0);				  //NO ACK.
+	if (r != 0)
+		return  FPGA_ERROR;
 
-	r = FPGA_IPM_open(
-				AES_CORE_ID,		  //AES_ID.
-				DECRYPT_OPCODE,       //DECRYPT OPERATION.
-				0,					  //POLLING MODE.
-				0);				      //NO ACK.
-	if (r == 0)
-		return  AES_FPGA_OPEN_ERROR;
+	//Write the lock.
+	FPGA_IPM_DATA lock = FPGA_LOCK;
+	FPGA_IPM_write(AES_CORE_ID,		   //AES_ID.
+				   LOCK_ADDR,	       //FPGA ADDRESS.
+				   &lock);		       //NO ACK.
 
-	//COM correctly opened.
-	//TODO - SEND ALL DATA SECTION ACCORDING WITH FPGA AND READ THE RESPONSE.
-	FPGA_IPM_write(AES_CORE_ID, 0, &encr_data);
-	FPGA_IPM_read(AES_CORE_ID, 0, &clear_data);
-	FPGA_IPM_close(AES_CORE_ID);
+	//Wait until FPGA ends the encryption.
+	while (lock != FPGA_LOCK)
+		FPGA_IPM_read(AES_CORE_ID, LOCK_ADDR, &lock);
+
+	//FPGA computation finished, read back the encrypted data.
+	FPGA_IPM_DATA rcvd;
+	for (int i=0; i<8; i++){
+		FPGA_IPM_read(AES_CORE_ID, (base_addr + i), &rcvd);
+		plaintext[i]   = (uint8_t) ((rcvd >> 8) & 0x00FF);
+		plaintext[i+1] = (uint8_t) (rcvd & 0x00FF);
+	}
+
+	//Close transaction.
+	if (FPGA_IPM_close(AES_CORE_ID) != 0)
+		return FPGA_ERROR;
 
 	return AES_FPGA_RES_OK;
+
 }
