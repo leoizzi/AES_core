747e73023a699082f776c24bdef0041c73b0de17
Nicole Dai Prà <nicoledaipra96@gmail.com>
2021-08-17 16:59:08 +0200

    fixed data and key's bit organization

--- HDL/tb/tb_add_round_keys.vhd
+++ HDL/tb/tb_add_round_keys.vhd
@@ -13,8 +13,9 @@ architecture test of tb_add_round_keys is
 		);
 	end component add_round_keys;
 
-	signal data_in: std_logic_vector(127 downto 0) := X"4c7a9ae526d3198106f8cb662848e004";
-	signal key: std_logic_vector(127 downto 0) := X"0539b11776392cfe6ca354fa2a2388a0";
+	signal data_in: std_logic_vector(127 downto 0) := X"046681e5e0cb199a48f8d37a2806264c";
+	signal key: std_logic_vector(127 downto 0) := X"a0fafe1788542cb123a339392a6c7605";
+	signal expected_data: std_logic_vector(127 downto 0) := X"a49c7ff2689f352b6b5bea43026a5049";
 	signal data_out: std_logic_vector(127 downto 0);
 begin
 	dut: add_round_keys
@@ -23,5 +24,12 @@ begin
 			key => key,
 			data_out => data_out
 		);
+
+	process
+	begin
+		wait for 1 ns;
+		assert (data_out = expected_data) report "WRONG ADD ROUND KEY" severity FAILURE;
+		wait;
+	end process
 
 end test;
